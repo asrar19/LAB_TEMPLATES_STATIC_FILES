2ca4bf16ec6bc081fd4d328d7395b7f87b2e4402
asrar <asrar.rifae@hotmail.com>
2023-02-02 23:49:22 +0300

    Solv lab

--- KingdomOfSaudiArabia/saudi_cites/urls.py
+++ KingdomOfSaudiArabia/saudi_cites/urls.py
@@ -9,5 +9,9 @@ urlpatterns = [
     path("tabuk/", views.tabuk, name="tabuk"),
     path("aldhahran/", views.aldhahran, name="aldhahran"),
     path("alaula/", views.alaula, name="alaula"),
+    path("consent/tos/", views.consent_to_TOS, name="consent_to_tos"),
+    path("mode/dark/", views.set_dark_mode, name="mode_dark"),
+    path("mode/light/", views.set_light_mode, name="mode_light"),
+
 ]
 
--- KingdomOfSaudiArabia/saudi_cites/views.py
+++ KingdomOfSaudiArabia/saudi_cites/views.py
@@ -1,13 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, resolve_url, redirect
 from django.http import HttpRequest, HttpResponse
 from datetime import date
+import random
+import string
+
+# Create your views here.
+
+def home_page(request : HttpRequest):
+    today = date.today()
+    context = {"today" : today}
+    cookies = request.COOKIES
+    riyadh_page_url = resolve_url("saudi_cites:riyadh")
+    response =  render(request, "saudi_cites/home_page.html",context )
+    response.set_cookie("last_visit", today)
+    return response,render(request, 'saudi_cites/home_page.html')
+
+
+
+
 
 
 
 def home_page(request : HttpRequest):
     today = date.today()
     context = {"today" : today}
-    return render(request, "saudi_cites/home_page.html", context )
+
+    cookies = request.COOKIES
+    return render(request, "saudi_cites/home_page.html",context )
+
+
+
+def consent_to_TOS(request : HttpRequest):
+    response = redirect("saudi_cites:home_page")
+    response.set_cookie("consent", "Yes", max_age=60*60*24*7)
+    return response
+
+
+
+
+def set_dark_mode(request : HttpRequest):
+    response = redirect("saudi_cites:home_page")
+    response.set_cookie("mode", "dark", max_age=60*60*24*7)
+    return response
+
+
+def set_light_mode(request : HttpRequest):
+    response = redirect("saudi_cites:home_page")
+    response.set_cookie("mode", "light", max_age=60*60*24*7)
+    return response
 
 
 
@@ -23,5 +63,12 @@ def tabuk(request : HttpRequest):
 
 def aldhahran(request : HttpRequest):
     return render(request, "saudi_cites/aldhahran.html")
+
+
+
+
+
+
+
 
 
